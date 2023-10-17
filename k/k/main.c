70f1d2db81f5a1ce98d8f6decdc32311a3749922
Logan <clarklwilliamson@gmail.com>
2023-10-17 13:15:24 -0700

    sure

---
+++ k/k/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  k
+//
+//  Created by Logan on 10/17/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
